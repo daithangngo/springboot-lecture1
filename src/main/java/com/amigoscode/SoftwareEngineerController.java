c293ca65aed28795f0a0baa8b219176ff60dad3d
Dai-Thang Ngo <daith.ngo@gmail.com>
2025-09-05 16:12:54 +0200

    succcessful created a simple api where we can add and get new Software engineers to a docker database

--- src/main/java/com/amigoscode/SoftwareEngineerController.java
+++ src/main/java/com/amigoscode/SoftwareEngineerController.java
@@ -1,8 +1,6 @@
 package com.amigoscode;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,12 +9,24 @@ import java.util.List;
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
 
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
     public List<SoftwareEngineer> getEngineers() {
-        return List.of(
-                new SoftwareEngineer(1, "James", "js, node, react, tailwindcss"),
-                new SoftwareEngineer(2, "Hanna", "java, spring, react, tailwindcss")
+        return softwareEngineerService.getSoftwareEngineers();
+    }
 
-        );
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineersById(@PathVariable Integer id) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
+    @PostMapping
+    public void addSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
     }
 }
